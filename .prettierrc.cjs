0e3d66eef64e5b2255fa1f78a0db2ffe1844e112
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-18 15:35:46 -0500

    Basic eslint/prettier setup

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    "arrowParens": "avoid",
+    "printWidth": 150,
+    "trailingComma": "all",
+    "semi": true,
+    "singleQuote": true,
+    "jsxSingleQuote": false,
+    "bracketSpacing": false,
+    "useTabs": true
+}
