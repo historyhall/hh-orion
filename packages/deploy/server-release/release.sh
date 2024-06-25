2bdd5c834a2f5510ef1238346f06a4226f67c841
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-25 18:28:01 -0500

    Update release.sh

--- packages/deploy/server-release/release.sh
+++ packages/deploy/server-release/release.sh
@@ -6,11 +6,11 @@ cp -n ./web/.default.env ./web.env
 cp -n ./server/.default.env ./server.env
 
 cd ./web
-sudo docker-compose pull
-sudo docker-compose down
-sudo docker-compose up -d
+sudo docker compose pull
+sudo docker compose down
+sudo docker compose up -d
 
 cd ./server
-sudo docker-compose pull
-sudo docker-compose down
-sudo docker-compose up -d
+sudo docker compose pull
+sudo docker compose down
+sudo docker compose up -d
