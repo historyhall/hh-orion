fc6303a78c77ff0720e27afc39633e38ce5b20ca
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-25 12:16:43 -0500

    feat: docker release workflow

--- packages/deploy/server/setup.sh
+++ packages/deploy/server/setup.sh
@@ -4,19 +4,11 @@
 sudo apt update -y;
 sudo apt upgrade -y;
 
-sudo apt install -y nginx certbot python3-certbot-nginx;
+sudo apt install -y nginx certbot python3-certbot-nginx apache2-utils;
 
 # Docker
 curl -fsSL https://get.docker.com -o get-docker.sh;
 sudo sh get-docker.sh;
-
-# Docker Registry
-sudo mkdir -p /srv/docker-registry/registry;
-sudo mkdir -p /srv/docker-registry/postgres;
-
-sudo cp ./docker-compose.yml /srv/docker-registry/;
-
-sudo docker compose --project-directory /srv/docker-registry up -d;
 
 # nginx
 sudo cp ./historyhall.org /etc/nginx/sites-available/historyhall.org
@@ -26,3 +18,16 @@ sudo systemctl restart nginx
 # certificate
 sudo certbot --nginx -d historyhall.org -d www.historyhall.org
 sudo certbot --nginx -d registry.historyhall.org -d www.registry.historyhall.org
+
+# Docker Registry
+sudo mkdir -p /srv/docker-registry/registry;
+sudo mkdir -p /srv/docker/postgres;
+
+sudo cp ./docker-compose.yml /srv/docker-registry/;
+
+# Registry authentication
+sudo mkdir /srv/docker-registry/auth
+cd /srv/docker-registry/auth
+sudo htpasswd -Bc registry.password dockerreguser
+
+sudo docker compose --project-directory /srv/docker-registry up -d;
