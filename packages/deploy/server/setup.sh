099f132346a562824c204633b12c1b851a5e324f
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-25 11:31:31 -0500

    fix: ssl

--- packages/deploy/server/setup.sh
+++ packages/deploy/server/setup.sh
@@ -4,7 +4,7 @@
 sudo apt update -y;
 sudo apt upgrade -y;
 
-sudo apt install -y nginx;
+sudo apt install -y nginx certbot;
 
 # Docker
 curl -fsSL https://get.docker.com -o get-docker.sh;
@@ -13,13 +13,12 @@ sudo sh get-docker.sh;
 # Docker Registry
 sudo mkdir -p /srv/docker-registry/registry;
 sudo mkdir -p /srv/docker-registry/postgres;
-cd /srv/docker-registry;
 
-cp ./docker-compose.yml /srv/docker-registry/;
+sudo cp ./docker-compose.yml /srv/docker-registry/;
 
-sudo docker-compose up -d;
+sudo docker compose --project-directory /srv/docker-registry up -d;
 
 # nginx
-sudo ./historyhall.org /etc/nginx/sites-available/historyhall.org
+sudo cp ./historyhall.org /etc/nginx/sites-available/historyhall.org
 sudo ln -s /etc/nginx/sites-available/historyhall.org /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
