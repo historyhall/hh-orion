660f9c4afe66267da5796376ecb2abd88b87ca7d
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-24 21:56:02 -0500

    Merge duplicate scripts

---
+++ packages/deploy/server/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update Packages
+sudo apt update -y;
+sudo apt upgrade -y;
+
+sudo apt install -y nginx;
+
+# Docker
+curl -fsSL https://get.docker.com -o get-docker.sh;
+sudo sh get-docker.sh;
+
+# Docker Registry
+sudo mkdir -p /srv/docker-registry/registry;
+sudo mkdir -p /srv/docker-registry/postgres;
+cd /srv/docker-registry;
+
+cp ./docker-compose.yml /srv/docker-registry/;
+
+sudo docker-compose up -d;
+
+# nginx
+sudo ./historyhall.org /etc/nginx/sites-available/historyhall.org
+$ sudo ln -s /etc/nginx/sites-available/historyhall.org /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
--- packages/deploy/setup-server-tools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update -y && apt upgrade -y;
-sudo apt install -y nginx;
-curl -fsSL https://get.docker.com -o get-docker.sh;
-sudo sh get-docker.sh;
