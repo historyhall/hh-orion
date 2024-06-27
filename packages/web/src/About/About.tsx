4afaff0bbda633225367489da00d86955ac86f3b
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-27 18:18:43 -0500

    Add use fetch hook

--- packages/web/src/About/About.tsx
+++ packages/web/src/About/About.tsx
@@ -1,18 +1,3 @@
-import {useEffect, useState} from "react";
-
 export function About() {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:5001/user/get-all')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setData(data);
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-    }, []);
-    return <>About: {JSON.stringify(data)}</>;
+    return <>About</>;
 }
---
+++ packages/web/src/server.ts
@@ -0,0 +1,29 @@
+import {useEffect, useState} from 'react';
+
+export function useFetch(path: string) {
+	const serverURL = process.env.REACT_APP_API_URL || 'https://historyhall.org';
+	const [data, setData] = useState<unknown>();
+	const [loading, setLoading] = useState<boolean>(false);
+	const [error, setError] = useState<string | undefined>();
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setLoading(true);
+			try {
+				const response = await fetch(`${serverURL}/${path}`);
+				if (!response.ok) {
+					throw new Error(response.statusText);
+				}
+				const json = await response.json();
+				setLoading(false);
+				setData(json);
+				setError(undefined);
+			} catch (error) {
+				setError(`${error} Could not Fetch Data `);
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, [path, serverURL]);
+	return {data, loading, error};
+}
