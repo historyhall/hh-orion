fc343febb4b15ac8fb84e806bbea6d2f0d351186
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-26 19:16:40 -0500

    fix: code

--- tools/fix-auto.ts
+++ tools/fix-auto.ts
@@ -1,13 +1,11 @@
 import type {FileInfo, API} from 'jscodeshift';
 import {fixDebugNamespace} from './lib/fixDebugNamespace';
-import {renameAliasImports} from './lib/renameAliasImports';
 import {sortImports} from './lib/sortImports';
 
-export default function transform(fileInfo: FileInfo, api: API) {
+export default function fixAuto(fileInfo: FileInfo, api: API) {
     const j = api.jscodeshift;
     const root = j(fileInfo.source);
 
-    renameAliasImports(root, j, fileInfo);
     sortImports(root, j);
     fixDebugNamespace(root, fileInfo);
 
