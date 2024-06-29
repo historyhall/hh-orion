d72a6d0505c2f07d1683cf99c253827da35347bf
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-29 08:35:19 -0500

    Sql scripts (#66)
    * Add backup scripts
    
    * Update scripts
    
    * Fixed nginx headers
    
    * Update release.yml

---
+++ packages/deploy/server-backup/pull_db.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+source .env
+
+sql="postgres://$DB_USERNAME:$DB_PASSWORD@$DB_DOMAIN:$DB_PORT/$DB_NAME"
+
+pg_dump "$sql" > history_hall.sql
---
+++ packages/deploy/server-backup/push_db.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+SOURCE .env
+
+sql="postgres://$DB_USERNAME:$DB_PASSWORD@$DB_DOMAIN:$DB_PORT/$DB_NAME"
+
+psql "$sql" <<EOF
+  DO \$\$ DECLARE
+  r RECORD;
+  BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+      EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+  END \$\$;
+EOF
+
+psql "$sql" < history_hall.sql
