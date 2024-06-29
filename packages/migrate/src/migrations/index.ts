66fab9dcc5c7c182c7a7b8214dce513b93c3ac7c
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-29 17:20:23 -0500

    Made migrations more resilient
    Closed #67

--- packages/migrate/src/migrations/index.ts
+++ packages/migrate/src/migrations/index.ts
@@ -1,10 +1,7 @@
 import {m20240619} from "./m20240619";
 import {m20240627} from "./m20240627";
 import {m20240628} from "./m20240628";
+import {Migration} from "../types";
+import {m20240629} from "./m20240629";
 
-type Migration = {
-    name: string;
-    action: string[];
-}
-
-export const migrations: Migration[] = [m20240619, m20240627, m20240628];
+export const migrations: Migration[] = [m20240619, m20240627, m20240628, m20240629];
--- packages/migrate/src/migrations/m20240628.ts
+++ packages/migrate/src/migrations/m20240628.ts
@@ -1,7 +1,8 @@
-export const m20240628 = {
+import {Migration} from "../types";
+
+export const m20240628: Migration = {
     name: 'm20240628',
-    action: [
-        `BEGIN;
+    action: `BEGIN;
             create table "author" ("id" uuid not null, "version" int not null default 1, "created_at" date not null, "first_name" text not null, "last_name" text not null, constraint "author_pkey" primary key ("id"));
             create table "document" ("id" uuid not null, "version" int not null default 1, "created_at" date not null, "name" text not null, "bytes" int not null, "storage_path" text not null, "filename" text not null, constraint "document_pkey" primary key ("id"));
             create table "document_authors" ("document_id" uuid not null, "author_id" uuid not null, constraint "document_authors_pkey" primary key ("document_id", "author_id"));
@@ -10,6 +11,5 @@ export const m20240628 = {
             alter table "document_authors" add constraint "document_authors_author_id_foreign" foreign key ("author_id") references "author" ("id") on update cascade on delete cascade;
             alter table "user_authors" add constraint "user_authors_user_id_foreign" foreign key ("user_id") references "user" ("id") on update cascade on delete cascade;
             alter table "user_authors" add constraint "user_authors_author_id_foreign" foreign key ("author_id") references "author" ("id") on update cascade on delete cascade;
-        COMMIT;`
-    ],
+        COMMIT;`,
 }
