1214ea3a5630f1d3390eb0e6a54a1aea343269b6
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-29 17:11:05 -0500

    Add secondary menu items (#68)
    * fix: access controls
    
    * Add support for secondary menu items
    
    * Fixed imports

--- packages/web/src/Layout/components/MenuSubItem.tsx
+++ packages/web/src/Layout/components/MenuSubItem.tsx
@@ -1,6 +1,6 @@
 import {Fragment} from "react";
 import {Link} from "react-router-dom";
-import {Icon, Image, MenuItem} from "semantic-ui-react";
+import {Dropdown, DropdownItem, DropdownMenu, Icon, Image, MenuItem} from "semantic-ui-react";
 import {Menu} from "../../types";
 
 interface Props {
@@ -13,16 +13,38 @@ export function MenuSubItem({menu, path}: Props) {
         <Fragment>
             {menu.image && (
                 <Link to={path}>
-                    <MenuItem position={menu.position}><Image src={menu.image} verticalAlign="middle" width={20} height={20} /></MenuItem>
+                    <MenuItem>
+                        <Image src={menu.image} verticalAlign="middle" width={20} height={20} />
+                    </MenuItem>
                 </Link>
             )}
             {(menu.name || menu.icon) && (
-                <Link to={path}>
-                    <MenuItem color="yellow" position={menu.position}>
-                        {menu.icon && <Icon name={menu.icon} />}
-                        {menu.name && <>{menu.name}</>}
-                    </MenuItem>
-                </Link>
+                <>
+                    {menu.secondaryMenuItem && (
+                        <Dropdown item text={menu.name} icon={menu.icon}>
+                            <DropdownMenu>
+                                {menu.secondaryMenuItem.map(secondary => {
+                                    return (
+                                        <DropdownItem key={secondary.path}>
+                                            <Link to={secondary.path} style={{color: 'black'}}>
+                                                    {secondary.icon && <Icon name={secondary.icon} />}
+                                                    {secondary.name && <>{secondary.name}</>}
+                                            </Link>
+                                        </DropdownItem>
+                                    );
+                                })}
+                            </DropdownMenu>
+                        </Dropdown>
+                    )}
+                    {!menu.secondaryMenuItem && (
+                        <Link to={path}>
+                            <MenuItem>
+                                {menu.icon && <Icon name={menu.icon} />}
+                                {menu.name && <>{menu.name}</>}
+                            </MenuItem>
+                        </Link>
+                    )}
+                </>
             )}
         </Fragment>
     );
--- packages/web/src/Profile/index.ts
+++ packages/web/src/Profile/index.ts
@@ -1,5 +1,8 @@
+import {Logout} from "../Layout/Logout";
 import {Page} from "../types";
+import {Login} from "./Login";
 import {Profile} from "./Profile";
+import {Register} from "./Register";
 
 const pages: Record<string, Page> = {
     profile: {
@@ -10,9 +13,50 @@ const pages: Record<string, Page> = {
         menu: {
             icon: 'user',
             position: 'right',
+            secondaryMenuItem: [
+                {
+                    icon: 'sign in',
+                    name: 'Login',
+                    path: '/profile/login',
+                },
+                {
+                    icon: 'signup',
+                    name: 'Register',
+                    path: '/profile/register',
+                },
+                // {
+                //     icon: 'sign out',
+                //     name: 'Logout',
+                //     path: '/profile/logout',
+                // },
+            ]
         },
         path: '/profile',
         component: Profile
+    },
+    login: {
+        header: {
+            name: 'Login',
+            icon: 'sign in'
+        },
+        path: '/profile/login',
+        component: Login
+    },
+    logout: {
+        header: {
+            name: 'Logout',
+            icon: 'sign out'
+        },
+        path: '/profile/logout',
+        component: Logout
+    },
+    register: {
+        header: {
+            name: 'Register',
+            icon: 'signup'
+        },
+        path: '/profile/register',
+        component: Register
     }
 }
 export default pages;
