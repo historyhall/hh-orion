662067cb18dd098666c9dcc3e5492cace3ebcbfc
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-29 17:20:39 -0500

    Update index.ts

--- packages/migrate/src/migrations/index.ts
+++ packages/migrate/src/migrations/index.ts
@@ -1,7 +1,7 @@
+import {Migration} from "../types";
 import {m20240619} from "./m20240619";
 import {m20240627} from "./m20240627";
 import {m20240628} from "./m20240628";
-import {Migration} from "../types";
 import {m20240629} from "./m20240629";
 
 export const migrations: Migration[] = [m20240619, m20240627, m20240628, m20240629];
