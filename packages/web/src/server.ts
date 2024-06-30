978e21bc9aa6160aef5f6c45b6d05a4f882fccc8
Shayne Thiessen <shayne@shaynethiessen.com>
2024-06-30 16:38:08 -0500

    Update server.ts (#83)
    * Update server.ts
    
    * Update server.ts

--- packages/web/src/server.ts
+++ packages/web/src/server.ts
@@ -18,7 +18,7 @@ export function useFetch<T>(path: string, params?: string[]): {data?: T; loading
 
 				let url = `${serverURL}/${path}`;
 
-				if(paramList) url += '?' + paramList;
+				if (paramList) url += '?' + paramList;
 				const response = await fetch(url);
 				if (!response.ok) {
 					throw new Error(response.statusText);
@@ -33,6 +33,6 @@ export function useFetch<T>(path: string, params?: string[]): {data?: T; loading
 			}
 		};
 		fetchData().catch(setError);
-	}, [path, serverURL]);
+	}, [path, serverURL, params]);
 	return {data, loading, error};
 }
