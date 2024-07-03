68367b02e28b23ea53f15754a80208133971e4a4
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-02 19:30:00 -0500

    Add form validation

--- packages/web/src/Profile/Login.tsx
+++ packages/web/src/Profile/Login.tsx
@@ -1,15 +1,30 @@
-import {Button, Form, FormField} from "semantic-ui-react";
+import {Button, Form, FormField, Input, Message} from "semantic-ui-react";
+import { useState} from "react";
 
 export function Login() {
+    const [errorMessage, setErrorMessage] = useState("");
+    const [inputs, setInputs] = useState<{email?: string, password?: string}>({})
+
+    function handleSubmit() {
+        if(!inputs.email) {
+            setErrorMessage("Please enter a valid email");
+        } else if(!inputs.password) {
+            setErrorMessage("Please enter a password");
+        } else {
+            setErrorMessage("");
+        }
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
+            {errorMessage && <Message header="Login Error" content={errorMessage} negative />}
             <FormField>
                 <label>Email</label>
-                <input placeholder='Email' />
+                <Input placeholder='Email' value={inputs.email} onChange={event => setInputs({...inputs, email: event.target.value})} />
             </FormField>
             <FormField>
                 <label>Password</label>
-                <input placeholder='Password' />
+                <Input placeholder='Password' type="password" value={inputs.password} onChange={event => setInputs({...inputs, password: event.target.value})}/>
             </FormField>
             <Button type='submit'>Login</Button>
         </Form>
--- packages/web/src/Profile/Register.tsx
+++ packages/web/src/Profile/Register.tsx
@@ -1,29 +1,50 @@
-import {Button, Form, FormField} from "semantic-ui-react";
+import {Button, Form, FormField, Input, Message} from "semantic-ui-react";
+import {useState} from "react";
 
 export function Register() {
+    const [errorMessage, setErrorMessage] = useState("");
+    const [inputs, setInputs] = useState<{firstName?: string, lastName?: string, email?: string, password1?: string, password2?: string}>({})
+
+    function handleSubmit() {
+        if(!inputs.firstName) {
+            setErrorMessage("Please enter your first name");
+        } else if(!inputs.lastName) {
+            setErrorMessage("Please enter your second name");
+        } else if(!inputs.email) {
+            setErrorMessage("Please enter a valid email");
+        } else if(!inputs.password1) {
+            setErrorMessage("Please enter a password");
+        } else if(inputs.password1 !== inputs.password2) {
+            setErrorMessage("Your passwords do not match");
+        } else {
+            setErrorMessage("");
+        }
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
+            {errorMessage && <Message header="Login Error" content={errorMessage} negative />}
             <FormField>
                 <label>First Name</label>
-                <input placeholder='Email' />
+                <Input placeholder='First Name' value={inputs.firstName} onChange={event => setInputs({...inputs, firstName: event.target.value})} />
             </FormField>
             <FormField>
                 <label>Last Name</label>
-                <input placeholder='Email' />
+                <Input placeholder='Last Name' value={inputs.lastName} onChange={event => setInputs({...inputs, lastName: event.target.value})} />
             </FormField>
             <FormField>
                 <label>Email</label>
-                <input placeholder='Email' />
+                <Input placeholder='Email' value={inputs.email} onChange={event => setInputs({...inputs, email: event.target.value})} />
             </FormField>
             <FormField>
                 <label>Password</label>
-                <input placeholder='Password' />
+                <Input placeholder='Password' value={inputs.password1} onChange={event => setInputs({...inputs, password1: event.target.value})} />
             </FormField>
             <FormField>
                 <label>Confirm Password</label>
-                <input placeholder='Password' />
+                <Input placeholder='Confirm Password' value={inputs.password2} onChange={event => setInputs({...inputs, password2: event.target.value})} />
             </FormField>
-            <Button type='submit'>Register</Button>
+            <Button type='submit' onClick={handleSubmit}>Register</Button>
         </Form>
     );
 }
