2ffb5490604bfa05698eb930afb84f81251d13cc
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-08 18:46:29 -0500

    Update index.ts

--- packages/server/src/system/index.ts
+++ packages/server/src/system/index.ts
@@ -1,8 +1,10 @@
 import {EntityManager} from "@mikro-orm/core";
 import {Migration} from "./Migration";
+import {Search} from './Search';
 
 export function System(em: EntityManager) {
     return [
         ...Migration(em),
+        ...Search(em),
     ]
 }
