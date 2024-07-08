a61bced034401f65f09d837f4931c34309221bd3
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-08 18:43:18 -0500

    Create SearchController.ts

---
+++ packages/domain/src/system/controllers/SearchController.ts
@@ -0,0 +1,16 @@
+import {EntityManager} from "@mikro-orm/core";
+import {Migration} from "../entities";
+
+export class SearchController {
+    public em;
+
+    public constructor(em: EntityManager) {
+        this.em = em.getRepository(Migration);
+    }
+
+    search() {
+    }
+    
+    indexDocuments() {
+    }
+}
