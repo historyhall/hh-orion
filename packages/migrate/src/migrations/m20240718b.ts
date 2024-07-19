8706282a33706bdba50462318a163ee1a214c5fc
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-19 08:45:09 -0500

    Fix Lint

--- packages/migrate/src/migrations/m20240718b.ts
+++ packages/migrate/src/migrations/m20240718b.ts
@@ -1,9 +1,9 @@
-import {Migration} from "../types";
+import {Migration} from '../types';
 
 export const m20240718b: Migration = {
-    name: 'm20240718b',
-    action: `BEGIN;
+	name: 'm20240718b',
+	action: `BEGIN;
                 DELETE FROM session;
                 alter table "session" alter column "expiry_date" type timestamptz using ("expiry_date"::timestamptz);
             COMMIT;`,
-}
+};
--- packages/schema/src/accounts/session/Session.ts
+++ packages/schema/src/accounts/session/Session.ts
@@ -1,10 +1,10 @@
-import {User} from "../user/User";
+import {User} from '../user/User';
 
 export type Session = {
-    id: string;
-    expiryDate: Date;
-    user: User;
-    token: string;
-    ipAddress: string;
-    agent: string;
-}
+	id: string;
+	expiryDate: Date;
+	user: User;
+	token: string;
+	ipAddress: string;
+	agent: string;
+};
