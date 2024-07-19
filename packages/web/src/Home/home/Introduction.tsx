f260081749118bed9185fc89b15f7cd8156f59ae
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-18 20:20:19 -0500

    Update content layout

---
+++ packages/web/src/Home/home/Introduction.tsx
@@ -0,0 +1,19 @@
+import {Grid, GridColumn, GridRow, Header} from "semantic-ui-react";
+
+export function Introduction() {
+    return (
+        <Grid stackable container>
+            <GridRow columns={1}>
+                <GridColumn>
+                    <Header content="Welcome to History Hall" />
+                </GridColumn>
+            </GridRow>
+            <GridRow columns={1}>
+                <GridColumn>
+                    <p>We are an advanced research database and virtual museum. Start searching below to explore
+                    our virtual halls.</p>
+                </GridColumn>
+            </GridRow>
+        </Grid>
+    );
+}
