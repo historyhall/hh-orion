7a19cf13bb678fff52f9d75591b8b5ba31ac9516
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-21 10:36:50 -0500

    Display map

--- packages/web/src/Explore/Explore.tsx
+++ packages/web/src/Explore/Explore.tsx
@@ -1,11 +1,18 @@
 import {MapContainer, TileLayer} from 'react-leaflet';
-import './explore.css';
-import 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
+import {Divider} from 'semantic-ui-react';
 
 export function Explore() {
+	const position = {lat: 54, lng: -105};
+
 	return (
-		<MapContainer style={{width: '100px', height: '100px'}}>
-			<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-		</MapContainer>
+		<>
+			<MapContainer center={position} zoom={4} style={{width: '100%', height: '100vh', zIndex: 10, display: 'block'}} attributionControl={false}>
+				<TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}" />
+			</MapContainer>
+			<Divider />
+			<p style={{fontSize: '11px'}}>
+				Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC
+			</p>
+		</>
 	);
 }
