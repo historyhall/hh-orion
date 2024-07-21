6477e4bcdec88b106fa8a49e0f63bb4455674537
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-21 07:52:51 -0500

    Add mobile menu

--- packages/web/src/Home/index.ts
+++ packages/web/src/Home/index.ts
@@ -1,12 +1,10 @@
 import {Page} from '../types';
 import {Home} from './Home';
-import image from './home/logo.png';
 
 const pages: Record<string, Page> = {
 	home: {
 		menu: {
 			name: 'Home',
-			image,
 			icon: 'home',
 		},
 		path: '/',
--- packages/web/src/Layout/layout/MainMenu.tsx
+++ packages/web/src/Layout/layout/MainMenu.tsx
@@ -1,34 +1,59 @@
-import {Fragment} from 'react';
-import {Menu, MenuMenu} from 'semantic-ui-react';
+import {Dropdown, DropdownMenu, Image, Menu, MenuItem, MenuMenu} from 'semantic-ui-react';
 import {Page} from '../../types';
 import {MenuSubItem} from './MenuSubItem';
+import {useIsMobile} from '../../useIsMobile';
+import {MenuSubItemMobile} from './MenuSubItemMobile';
+import {Link} from 'react-router-dom';
+import logo from './logo.png';
 
 interface Props {
 	pages: Page[];
 }
+
 export function MainMenu({pages}: Props) {
+	const isMobile = useIsMobile();
 	const leftMenuItems = pages.filter(page => page.menu?.position === 'left' || page.menu?.position === undefined);
 	const rightMenuItems = pages.filter(page => page.menu?.position === 'right');
+
+	if (isMobile) {
+	}
+
 	return (
-		<Fragment>
-			<Menu attached inverted>
-				{leftMenuItems.map(page => {
+		<Menu attached inverted>
+			<Link to="/">
+				<MenuItem>
+					<Image src={logo} verticalAlign="middle" width={20} height={20} />
+				</MenuItem>
+			</Link>
+			{isMobile && (
+				<Dropdown item icon="bars" simple>
+					<DropdownMenu>
+						{leftMenuItems.map(page => {
+							if (page.menu) {
+								return <MenuSubItemMobile menu={page.menu} path={page.path} key={page.path} />;
+							}
+							return null;
+						})}
+					</DropdownMenu>
+				</Dropdown>
+			)}
+			{!isMobile &&
+				leftMenuItems.map(page => {
 					if (page.menu) {
 						return <MenuSubItem menu={page.menu} path={page.path} key={page.path} />;
 					}
 					return null;
 				})}
-				{rightMenuItems.length > 0 && (
-					<MenuMenu position="right">
-						{rightMenuItems.map(page => {
-							if (page.menu) {
-								return <MenuSubItem menu={page.menu} path={page.path} key={page.path} />;
-							}
-							return null;
-						})}
-					</MenuMenu>
-				)}
-			</Menu>
-		</Fragment>
+			{rightMenuItems.length > 0 && (
+				<MenuMenu position="right">
+					{rightMenuItems.map(page => {
+						if (page.menu) {
+							return <MenuSubItem menu={page.menu} path={page.path} key={page.path} />;
+						}
+						return null;
+					})}
+				</MenuMenu>
+			)}
+		</Menu>
 	);
 }
