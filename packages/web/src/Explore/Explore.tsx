bcc9064052a11ea5dfaf4ed570df6ae622dd375a
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-21 09:46:56 -0500

    Add leaflet

--- packages/web/src/Explore/Explore.tsx
+++ packages/web/src/Explore/Explore.tsx
@@ -1,7 +1,11 @@
+import {MapContainer, TileLayer} from 'react-leaflet';
+import './explore.css';
+import 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js';
+
 export function Explore() {
 	return (
-		<>
-			Explore
-		</>
+		<MapContainer style={{width: '100px', height: '100px'}}>
+			<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+		</MapContainer>
 	);
 }
