24a0a4c92e6adafcbba379effa1466b7bedf890d
Shayne Thiessen <shayne@shaynethiessen.com>
2024-07-26 19:43:47 -0500

    Limit migration tasks to once per day (#204)
    * Fix failed migrations
    
    * Limit maintenance jobs to once a day
    
    * Fixed user verification maintenance

--- packages/migrate/src/maintenance/removeUnverifiedUsers.ts
+++ packages/migrate/src/maintenance/removeUnverifiedUsers.ts
@@ -2,5 +2,5 @@ import {Migration} from '../types';
 
 export const removeUnverifiedUsers: Migration = {
 	name: 'removeUnverifiedUsers',
-	action: `DELETE FROM user where expiry_date > CURRENT_TIMESTAMP + INTERVAL '30' day;`,
+	action: `DELETE FROM "user" where created_at > CURRENT_TIMESTAMP + INTERVAL '30' day;`,
 };
