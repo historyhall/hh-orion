92b3f6934d54a69627106d813b4b9c93b2bf9da7
Shayne Thiessen <shayne@shaynethiessen.com>
2024-08-15 09:56:22 -0500

    Add more logging

--- packages/file-upload/src/lib/writeToStorage.ts
+++ packages/file-upload/src/lib/writeToStorage.ts
@@ -3,6 +3,9 @@ import * as fs from 'node:fs';
 import {join} from 'node:path';
 import {Readable} from 'node:stream';
 import {randomString} from './randomString';
+import debug from 'debug';
+
+const d = debug('hh.file-upload.upload.writeToStorage');
 
 export async function writeToStorage(tmpStream: Readable, tmpFolder: string, extension: string): Promise<{filename: string; storagePath: string}> {
 	function pad(number: number, length: number) {
@@ -15,18 +18,22 @@ export async function writeToStorage(tmpStream: Readable, tmpFolder: string, ext
 		const year = new Date().getFullYear().toString();
 
 		const storagePath = join(tmpFolder, year, month, day);
+		d('Directory path:', storagePath);
 		if (!fs.existsSync(storagePath)) {
 			fs.mkdirSync(storagePath, {recursive: true});
 		}
 
 		const filename = randomString() + extension;
 		const fullPath = join(storagePath, filename);
+		d('Full path:', fullPath);
 
 		const writeStream = createWriteStream(fullPath);
 		writeStream.on('close', () => {
+			d('Stream closed');
 			resolve({filename, storagePath});
 		});
 		writeStream.on('error', error => {
+			d('Stream error');
 			reject(error);
 		});
 		tmpStream.pipe(writeStream);
